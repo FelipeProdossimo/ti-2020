6278ed5644151e37f171ef15c6f1ded8baa3878d
Guto Xavier <gutoffline@gmail.com>
2020-12-09 21:29:46 -0300

    Aula 05

---
+++ UC-14/JAVASCRIPT/Calculadora/js/funcoes.js
@@ -0,0 +1,35 @@
+function Inserir7(){
+    document.getElementById("visor").value += 7;
+}
+
+function Inserir8(){
+    document.getElementById("visor").value += 8;
+}
+
+function Inserir9(){
+    document.getElementById("visor").value += 9;
+}
+
+function InserirMais(){
+    document.getElementById("visor").value += "+";
+}
+
+function LimparVisor(){
+    document.getElementById("visor").value = "";
+}
+
+function RaizQuadrada(){
+    let numero = document.getElementById("visor").value;
+    document.getElementById("visor").value = Math.sqrt(numero);
+}
+
+function Calcular(){
+    //pegar o texto do visor
+    let expressao = document.getElementById("visor").value;
+
+    //converter em expressão numérica e calcular
+    let resultado = eval(expressao);
+
+    //mostrar o resultado do cálculo no visor no visor
+    document.getElementById("visor").value = resultado;
+}
